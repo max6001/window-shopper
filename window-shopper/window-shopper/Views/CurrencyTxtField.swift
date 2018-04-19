1ecd84fbace9785872dc16aae3ab556dbc1b8d2e
max6001 <maximilien.fagot@gmail.com>
2018-04-19 15:41:11 +0100

    UI Creation

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Maximilien Fagot on 19/04/2018.
+//  Copyright © 2018 Maximilien Fagot. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
